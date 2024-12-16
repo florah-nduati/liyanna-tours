825e4b38bff2472a95f4375ce3f28e1019755f84
git <florah@gmail.com>
2024-12-16 11:14:59 +0300

    json files

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '', // Leave this empty for relative paths
+  base: '', // Leave empty for root-level deployment
   build: {
     outDir: 'dist',
     emptyOutDir: true,
